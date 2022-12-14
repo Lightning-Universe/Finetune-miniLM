6fa44269f3a21e385948c3451369da8b8d8414fa
Carlos Mocholí <carlossmocholi@gmail.com>
2022-12-14 22:52:06 +0100

    curl --create-dirs

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #! pip install git+https://github.com/Lightning-AI/Finetune-miniLM
-#! mkdir -p ${HOME}/data/yelpreviewfull
-#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/train.csv -o ${HOME}/data/yelpreviewfull/train.csv -C -
-#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv -o ${HOME}/data/yelpreviewfull/test.csv -C -
+#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/train.csv --create-dirs -o ${HOME}/data/yelpreviewfull/train.csv -C -
+#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv --create-dirs -o ${HOME}/data/yelpreviewfull/test.csv -C -
 import lightning as L
 import torch
 from transformers import *
