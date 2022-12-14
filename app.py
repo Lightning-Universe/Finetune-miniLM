ada5206f139fd07417a53359fc122d6f56558b8f
Carlos Mocholí <carlossmocholi@gmail.com>
2022-12-14 22:15:08 +0100

    Do not download if the data exists already

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #! pip install git+https://github.com/Lightning-AI/Finetune-miniLM
 #! mkdir -p ${HOME}/data/yelpreviewfull
-#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/train.csv -o ${HOME}/data/yelpreviewfull/train.csv
-#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv -o ${HOME}/data/yelpreviewfull/test.csv
+#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/train.csv -o ${HOME}/data/yelpreviewfull/train.csv -C -
+#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv -o ${HOME}/data/yelpreviewfull/test.csv -C -
 import lightning as L
 import torch
 from transformers import *
