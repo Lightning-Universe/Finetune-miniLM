719a52dc42fec53dd6ec88c3e9a59fe6c95f2f42
Carlos Mocholí <carlossmocholi@gmail.com>
2022-12-14 23:54:08 +0100

    Fix local issues

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 #! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv --create-dirs -o ${HOME}/data/yelpreviewfull/test.csv -C -
 import lightning as L
 import torch
-from transformers import *
 from finetune_minilm import *
+import transformers
 
 
 class EmbeddingSimilarity(L.LightningModule):
@@ -18,23 +18,23 @@ class EmbeddingSimilarity(L.LightningModule):
         super().__init__()
         self.module = module
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch):
         x, y = batch
         embeddings = self.module(x)
         loss = pairwise_cosine_embedding_loss(embeddings, y)
-        self.log("train_loss", loss, prog_bar=True, on_step=False, on_epoch=True)
+        self.log("train_loss", loss, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
         return loss
 
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch, _):
         x, y = batch
         embeddings = self.module(x)
         loss = pairwise_cosine_embedding_loss(embeddings, y)
-        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_loss", loss, prog_bar=True, sync_dist=True)
         return loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=5e-5, weight_decay=0.001)
-        scheduler = get_linear_schedule_with_warmup(
+        scheduler = transformers.get_linear_schedule_with_warmup(
             optimizer, num_warmup_steps=5, num_training_steps=self.trainer.estimated_stepping_batches
         )
         return [optimizer], [scheduler]
@@ -53,15 +53,15 @@ class Finetune(L.LightningWork):
 
         L.seed_everything(777, workers=True)
         tokenizer = self.configure_tokenizer()
-        train_dataloader = self.configure_data("~/data/yelpreviewfull/train.csv", tokenizer)
-        val_dataloader = self.configure_data("~/data/yelpreviewfull/test.csv", tokenizer)
+        train_dataloader = self.configure_data("~/data/yelpreviewfull/train.csv", shuffle=True, batch_size=16, tokenizer=tokenizer)
+        val_dataloader = self.configure_data("~/data/yelpreviewfull/test.csv", batch_size=32, tokenizer=tokenizer)
         lightning_module = self.configure_module()
 
         trainer = L.Trainer(
             max_epochs=5,
             limit_train_batches=100,
             limit_val_batches=100,
-            strategy="ddp_find_unused_parameters_false",
+            strategy="ddp",
             precision=16,
             accelerator="auto",
             devices="auto",
@@ -77,10 +77,10 @@ class Finetune(L.LightningWork):
         return EmbeddingSimilarity(module)
 
     def configure_tokenizer(self):
-        return AutoTokenizer.from_pretrained("microsoft/MiniLM-L12-H384-uncased")
+        return transformers.AutoTokenizer.from_pretrained("microsoft/MiniLM-L12-H384-uncased")
 
-    def configure_data(self, path: str, tokenizer) -> torch.utils.data.DataLoader:
-        return TokenizedDataloader(dataset=TextDataset(csv_file=path), batch_size=16, shuffle=True, tokenizer=tokenizer)
+    def configure_data(self, path: str, **kwargs) -> torch.utils.data.DataLoader:
+        return TokenizedDataloader(dataset=TextDataset(csv_file=path), **kwargs)
 
     def configure_callbacks(self):
         early_stopping = L.pytorch.callbacks.EarlyStopping(monitor="val_loss", min_delta=0.00, verbose=True, mode="min")
