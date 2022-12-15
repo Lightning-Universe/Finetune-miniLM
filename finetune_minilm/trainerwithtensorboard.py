4520209d0afaf46e0a0204864cde8be68db8b9fb
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-12-16 00:09:51 +0100

    Fix Work stalling after MPS availability check (#2)

--- finetune_minilm/trainerwithtensorboard.py
+++ finetune_minilm/trainerwithtensorboard.py
@@ -11,6 +11,8 @@ class TrainerWithTensorboard(L.LightningFlow):
         tb_drive = L.app.storage.Drive("lit://tb_drive")
         self.tensorboard_work = TensorBoardWork(drive=tb_drive)
         self.trainer_work = work_cls(cloud_compute=cloud_compute, tb_drive=tb_drive)
+        # workaround for issue https://github.com/Lightning-AI/lightning/issues/16078
+        self.trainer_work._start_method = "spawn"
 
     def run(self) -> None:
         self.tensorboard_work.run()
